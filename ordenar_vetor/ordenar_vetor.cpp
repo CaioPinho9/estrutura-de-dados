ed061b4324f5762b4fee1b455faefe17de84cc2f
CaioPinho9 <caiopinho9@gmail.com>
2023-08-09 21:47:05 -0300

    feat: ordenar vetor por referencia

--- ordenar_vetor/ordenar_vetor.cpp
+++ ordenar_vetor/ordenar_vetor.cpp
@@ -5,9 +5,9 @@
 #include <vector>
 #include <iostream>
 
-bool is_sorted(std::vector<int> v) {
+bool is_sorted(std::vector<int>& v) {
     for (unsigned int i = 0; i < v.size() - 1; i++) {
-        if (v[i] > v[i + 1])
+        if (&v[i] > &v[i + 1])
             return false;
     }
     return true;
