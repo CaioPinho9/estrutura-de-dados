4e7e7978411b277bd1d112d4e2dc6f886c928e97
CaioPinho9 <caiopinho9@gmail.com>
2023-08-09 21:34:50 -0300

    feat: ordenar vetor

---
+++ ordenar_vetor/ordenar_vetor.cpp
@@ -0,0 +1,28 @@
+//
+// Created by caiop on 09/08/2023.
+//
+
+#include <vector>
+#include <iostream>
+
+bool is_sorted(std::vector<int> v) {
+    for (unsigned int i = 0; i < v.size() - 1; i++) {
+        if (v[i] > v[i + 1])
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    std::vector<int> v = {10, 20, 30, 40, 50};
+
+    for (auto x: v) {
+        std::cout << x << ' ';
+    }
+
+    std::cout << '\n';
+
+    std::cout << "Is sorted: " << is_sorted(v) << '\n';
+
+    return 0;
+}
