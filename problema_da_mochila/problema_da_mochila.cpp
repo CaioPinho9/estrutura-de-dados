9f45d17cb4f2ee8d58ddb5b18a7d4ce50d5d7c2b
CaioPinho9 <caiopinho9@gmail.com>
2023-08-17 19:42:59 -0300

    feat: incomplete selection sort

--- problema_da_mochila/problema_da_mochila.cpp
+++ problema_da_mochila/problema_da_mochila.cpp
@@ -6,31 +6,88 @@
 #include <vector>
 
 typedef struct Item {
+    int tag = 0;
     int value = 0;
     int weight = 0;
-    int number_of_items = 0;
-    double value_by_weight = 0;
+    int numberOfItems = 0;
+    double valueByWeight = 0;
 } Item;
 
 std::vector<Item> read() {
-    std::vector<Item> list_of_items;
-    int exit = 0;
+    std::vector<Item> listOfItems;
+    int exit;
+    int tag = 0;
     while (true) {
         Item item;
-        std::cin >> item.value >> item.weight >> item.number_of_items;
-        item.value_by_weight = static_cast<double>(item.value) / item.weight;
-        exit = item.value + item.weight + item.number_of_items;
+        std::cin >> item.value >> item.weight >> item.numberOfItems;
+        item.valueByWeight = static_cast<double>(item.value) / item.weight;
+        item.tag = tag;
+        tag++;
+        exit = item.value + item.weight + item.numberOfItems;
         if (exit == -3) {
             break;
         }
-        list_of_items.push_back(item);
+        listOfItems.push_back(item);
     }
-    return list_of_items;
+    return listOfItems;
 }
 
+typedef struct BackpackItem {
+    int itemType = 0;
+    int quantity = 0;
+} BackpackItem;
+
+//void selectionSort(std::vector<int> &v) {
+//    unsigned int n = v.size();
+//    for (int i = 0; i < n - 1; ++i) {
+//        unsigned int index_min = i;
+//        for (int j = i; j < n; ++j) {
+//            if (v[j] < v[index_min]) {
+//                index_min = j;
+//            }
+//        }
+//        auto aux = v[i];
+//        v[i] = v[index_min];
+//        v[index_min] = aux;
+//    }
+//}
+
+void fillBackpack(std::vector<BackpackItem> &backpack, std::vector<Item> &listOfItems, unsigned int backpackSize) {
+    // Selection sort algorithm
+    unsigned int backpackIndex = 0;
+
+    unsigned int n = listOfItems.size();
+    for (int i = 0; i < n - 1; ++i) {
+        unsigned int indexMax = i;
+        for (int j = i; j < n; ++j) {
+            if (listOfItems[j].valueByWeight > listOfItems[indexMax].valueByWeight) {
+                indexMax = j;
+            }
+        }
+        auto bestItem = listOfItems[indexMax];
+        listOfItems[indexMax] = listOfItems[i];
+        listOfItems[i] = bestItem;
+
+        while (backpackSize >= bestItem.weight and bestItem.numberOfItems > 0) {
+            if (backpack.size() < backpackIndex + 1) {
+                backpack.emplace_back();
+                backpack[backpackIndex].itemType = bestItem.tag;
+            }
+            backpack[backpackIndex].quantity++;
+            bestItem.numberOfItems--;
+        }
+        backpackIndex++;
+    }
+
+};
+
 int main() {
-    std::vector<Item> list_of_items;
-    list_of_items = read();
+    unsigned int backpackSize = 20;
+    std::vector<BackpackItem> backpack;
+    std::vector<Item> listOfItems;
+    listOfItems = read();
+
+    fillBackpack(backpack, listOfItems, backpackSize);
 
     return 0;
 }
